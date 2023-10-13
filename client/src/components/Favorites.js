954f78efc8ecc58ab4bb81b158fffd7ebfdfcf67
ItsMeAlec <alecsouthward@gmail.com>
2023-10-13 10:53:02 +0200

    modified:   client/src/components/Favorites.js 	modified:   client/src/components/ItunesSearch.js

--- client/src/components/Favorites.js
+++ client/src/components/Favorites.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -42,48 +41,3 @@ function Favorites() {
 }
 
 export default Favorites;
-=======
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-// Favorites component responsible for displaying favorite items
-function Favorites() {
-    // State variable to store favorite items retrieved from the server
-    const [favorites, setFavorites] = useState([]);
-
-    // useEffect hook to fetch favorites from the server when the component mounts
-    useEffect(() => {
-        const fetchFavorites = async () => {
-            try {
-                // Send a GET request to the server's /favorites endpoint to retrieve favorite items
-                const response = await axios.get('/favorites');
-                // Update the favorites state with the response data from the server
-                setFavorites(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        // Call the fetchFavorites function when the component mounts
-        fetchFavorites();
-    }, []); // Empty dependency array ensures that this effect runs once after the initial render
-
-    // Render favorite items
-    return (
-        <div>
-            <h1 style={{textAlign:'center'}}>Favorites</h1>
-            <div className="favorites-list">
-                {/* Map through favorite items and display track name, artwork, and artist name */}
-                {favorites.map((item) => (
-                    <div className='song' key={item.trackId}>
-                        <h2>{item.trackName}</h2>
-                        <img src={item.artworkUrl60} alt={'Artwork for ' + item.trackName}></img>
-                        <p>{item.artistName}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
-
-export default Favorites;
->>>>>>> a9746564701b9bf28550d2a070bdff1e48c3a263
--- client/src/components/ItunesSearch.js
+++ client/src/components/ItunesSearch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import './ItunesSearch.css';
 import axios from 'axios';
@@ -51,57 +50,3 @@ function ItunesSearch() {
 }
 
 export default ItunesSearch;
-=======
-import React, { useState } from "react";
-import './ItunesSearch.css';
-import axios from 'axios';
-
-// ItunesSearch component responsible for searching and displaying iTunes results
-function ItunesSearch() {
-    // State variables for search query and search results
-    const [query, setQuery] = useState('');
-    const [results, setResults] = useState([]);
-
-    // Function to search iTunes API based on the user's query
-    const searchiTunes = async () => {
-        try {
-            // Send a GET request to the server's /search endpoint with the user's query
-            const response = await axios.get(`/search/${query}`);
-            // Update the results state with the response data from the server
-            setResults(response.data.results);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    // Render the search bar, search button, and search results
-    return (
-        <div>
-            {/* Search bar and button */}
-            <div className='search-bar'>
-                <input
-                    type="text"
-                    placeholder="Search for content..."
-                    value={query}
-                    onChange={(e) => setQuery(e.target.value)}
-                />
-                <button onClick={searchiTunes}>Search</button>
-            </div>
-
-            {/* Display search results */}
-            <div className="results">
-                {results.map((item) => (
-                    <div className='song' key={item.trackId}>
-                        {/* Display track name, artwork, and artist name */}
-                        <h2>{item.trackName}</h2>
-                        <img src={item.artworkUrl60} alt={'Artwork for ' + item.trackName}></img>
-                        <p>{item.artistName}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
-
-export default ItunesSearch;
->>>>>>> a9746564701b9bf28550d2a070bdff1e48c3a263
